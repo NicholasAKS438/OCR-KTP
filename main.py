eef37e83f4ea65b467077d64b294511928213566
NicholasAKS438 <nicholas@akseleran.com>
2024-11-12 17:55:10 +0700

    Feat: Contrasting, prompt improvement and refactor

--- main.py
+++ main.py
@@ -17,6 +17,38 @@ load_dotenv()
 
 app = FastAPI()
 API_KEY = os.getenv("API_KEY")
+
+
+def cvt_BGR2RGB(img):
+  return cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+def contrast(img):
+  # CLAHE (Contrast Limited Adaptive Histogram Equalization)
+  clahe=cv2.createCLAHE(clipLimit=3., tileGridSize=(8,8))
+
+  lab=cv2.cvtColor(img, cv2.COLOR_BGR2LAB)  # convert from BGR to LAB color space
+  l,a,b=cv2.split(lab)  # split on 3 different channels
+
+  l2=clahe.apply(l)  # apply CLAHE to the L-channel
+
+  lab=cv2.merge((l2,a,b))  # merge channels
+  img2=cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)  # convert from LAB to BGR
+  return img2
+
+def flatten_image(array_img, mask_array):
+    hull = cv2.convexHull(mask_array)
+
+    # Approximate the convex hull to a quadrilateral
+    epsilon = 0.02 * cv2.arcLength(hull, True)  # Adjust the epsilon for more or less approximation
+    approx_quad = cv2.approxPolyDP(hull, epsilon, True)
+
+    # If the approximation has more than 4 points, adjust or retry with a different epsilon
+    if len(approx_quad) != 4:
+        print("Failed to approximate to quadrilateral; current approximation has", len(approx_quad), "points.")
+    else:
+        array_img = four_point_transform(array_img, approx_quad.reshape(4, 2))
+    return array_img
+
 def extractText(file):    
     genai.configure(api_key=API_KEY)
 
@@ -31,17 +63,9 @@ def extractText(file):
     mask_array = np.array(masks, dtype=np.int32)
     mask_array = mask_array.reshape((-1, 1, 2))  # Reshape for OpenCV
 
-    hull = cv2.convexHull(mask_array)
-
-    # Approximate the convex hull to a quadrilateral
-    epsilon = 0.02 * cv2.arcLength(hull, True)  # Adjust the epsilon for more or less approximation
-    approx_quad = cv2.approxPolyDP(hull, epsilon, True)
-
-    # If the approximation has more than 4 points, adjust or retry with a different epsilon
-    if len(approx_quad) != 4:
-        print("Failed to approximate to quadrilateral; current approximation has", len(approx_quad), "points.")
-    else:
-        dst = four_point_transform(dst, approx_quad.reshape(4, 2))
+    dst = flatten_image(dst,mask_array)
+    
+    dst = contrast(dst)
         
 
     dst = Image.fromarray(dst)
@@ -50,6 +74,7 @@ def extractText(file):
     [dst, "\n\n", """ 
     Ekstrak teks pada gambar dan identifikasi NIK, Nama, Tanggal Lahir dan Alamat yang terdiri dari Alamat, RT/RW, Kelurahan/Desa dan Kecamatan ke dalam format JSON seperti di bawah tanpa tambahan ```json```
         Tempat lahir tidak termasuk dalam tanggal lahir
+        Berikan null jika informasi teks blur atau susah diekstrak
         NIK hanya berjumlah 16 digit, tidak lebih dan tidak kurang, pastikan tidak melakukan output angka yang duplikat
         {
         "NIK": "0000000000000000",
@@ -67,7 +92,7 @@ def extractText(file):
     json_ktp = json.loads(text)
 
     #NOTE Remove if not every value has to be filled
-    if None in json_ktp.values() or "null" in json_ktp.values(): 
+    if None in json_ktp.values() or "null" in json_ktp.values() or None in json_ktp['Alamat'].values():  
         return {"detail":"Gambar tidak jelas"}
 
     if ("NIK" in json_ktp.keys() and len(json_ktp["NIK"]) != 16):
--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import numpy as np
 import cv2
 from imutils.perspective import four_point_transform
 import time
+import matplotlib.pyplot as plt
 
 from dotenv import load_dotenv
 from ultralytics import YOLO
@@ -18,6 +19,37 @@ load_dotenv()
 
 app = FastAPI()
 API_KEY = os.getenv("API_KEY")
+
+def cvt_BGR2RGB(img):
+  return cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+def contrast(img):
+  # CLAHE (Contrast Limited Adaptive Histogram Equalization)
+  clahe=cv2.createCLAHE(clipLimit=3., tileGridSize=(8,8))
+
+  lab=cv2.cvtColor(img, cv2.COLOR_BGR2LAB)  # convert from BGR to LAB color space
+  l,a,b=cv2.split(lab)  # split on 3 different channels
+
+  l2=clahe.apply(l)  # apply CLAHE to the L-channel
+
+  lab=cv2.merge((l2,a,b))  # merge channels
+  img2=cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)  # convert from LAB to BGR
+  return img2
+
+def flatten_image(array_img, mask_array):
+    hull = cv2.convexHull(mask_array)
+
+    # Approximate the convex hull to a quadrilateral
+    epsilon = 0.02 * cv2.arcLength(hull, True)  # Adjust the epsilon for more or less approximation
+    approx_quad = cv2.approxPolyDP(hull, epsilon, True)
+
+    # If the approximation has more than 4 points, adjust or retry with a different epsilon
+    if len(approx_quad) != 4:
+        print("Failed to approximate to quadrilateral; current approximation has", len(approx_quad), "points.")
+    else:
+        array_img = four_point_transform(array_img, approx_quad.reshape(4, 2))
+    return array_img
+
 def extractText(file):    
     start = time.time()
     genai.configure(api_key=API_KEY)
@@ -33,18 +65,10 @@ def extractText(file):
     mask_array = np.array(masks, dtype=np.int32)
     mask_array = mask_array.reshape((-1, 1, 2))  # Reshape for OpenCV
 
-    hull = cv2.convexHull(mask_array)
+    dst = flatten_image(dst,mask_array)
+    
+    dst = contrast(dst)
 
-    # Approximate the convex hull to a quadrilateral
-    epsilon = 0.02 * cv2.arcLength(hull, True)  # Adjust the epsilon for more or less approximation
-    approx_quad = cv2.approxPolyDP(hull, epsilon, True)
-
-    # If the approximation has more than 4 points, adjust or retry with a different epsilon
-    if len(approx_quad) != 4:
-        print("Failed to approximate to quadrilateral; current approximation has", len(approx_quad), "points.")
-    else:
-        dst = four_point_transform(dst, approx_quad.reshape(4, 2))
-        
     output_path = 'C:\\OCR-KTP\\OCRR\\OCR-KTP'
     cv2.imwrite(output_path+'/test.jpg', dst)
     print(f"Image saved as {output_path}")
@@ -55,13 +79,15 @@ def extractText(file):
     start = time.time()
     result = model_genai.generate_content(
     [dst, "\n\n", """ 
-    Ekstrak teks pada gambar dan NIK, Nama, Tanggal Lahir dan Alamat yang terdiri dari Alamat, RT/RW, Kelurahan/Desa dan Kecamatan ke dalam format JSON di bawah tanpa tambahan ```json```
-        NIK hanya berjumlah 16 digit, tidak lebih dan tidak kurang, baca teks lagi jika NIK yang dibaca oleh AI tidak mencapai 16 digit, ulangi sampai NIK mencapai 16 digit
+    Ekstrak teks pada gambar dan identifikasi NIK, Nama, Tanggal Lahir dan Alamat yang terdiri dari Alamat, RT/RW, Kelurahan/Desa dan Kecamatan ke dalam format JSON seperti di bawah tanpa tambahan ```json```
+        Tempat lahir tidak termasuk dalam tanggal lahir
+        Berikan null jika informasi teks blur atau susah diekstrak
+        NIK hanya berjumlah 16 digit, tidak lebih dan tidak kurang, pastikan tidak melakukan output angka yang duplikat
         {
-        "NIK": "1234567890111111",
+        "NIK": "0000000000000000",
         "Nama": "ABC",
         "Tanggal Lahir": "01-02-2000",
-        "Alamat": {"Alamat": "ABC", "RT/RW": "001/003", "Kelurahan/Desa": "ABC", "Kecamatan": "ABC"}
+        "Alamat": {"Alamat": "ABC", "RT/RW": "001/003", "Kelurahan/Desa": "ABC", "Kecamatan": "ABC"},
         }
     """]
     )
@@ -78,8 +104,7 @@ def extractText(file):
     billing = (result.usage_metadata.candidates_token_count*input_price/1000000) + (result.usage_metadata.prompt_token_count*output_price/1000000)
     print("Billing amount: " + str(billing))
     json_ktp = json.loads(text)
-
-    if None in json_ktp.values() or "null" in json_ktp.values(): 
+    if None in json_ktp.values() or "null" in json_ktp.values() or None in json_ktp['Alamat'].values():  
         return {"detail":"Gambar tidak jelas"}
 
     if ("NIK" in json_ktp.keys() and len(json_ktp["NIK"]) != 16):
